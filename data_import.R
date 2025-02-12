7562379385031d35efb6f1fa91e20525a561b3be
Mandip Chaudhary <mandip810@gmail.com>
2025-02-12 15:42:11 +0545

    feat: add data import and visualization script in R

---
+++ data_import.R
@@ -0,0 +1,24 @@
+# by default the r-studio will set working directory as the root folder
+# /home for mac
+# setting current project as working directory
+# setwd("~/Desktop/projects/r-starter")
+# if working directory is not set to current project, we will have to
+# give full file path to read the file even if file exists in same location
+# df = read.csv("/Users/admin/Desktop/projects/r-starter/covnep_252days.csv")
+df = read.csv("covnep_252days.csv")
+df
+length(df$totalCases)
+class(df$totalCases)
+summary(df)
+
+
+# choose from gui
+# df = read.csv(file.choose())
+# df
+# summary(df)
+
+
+plot(df$totalCases, df$totalRecoveries,
+     panel.first = grid(8, 8),
+     pch = 0, cex = 1.2, col = "black")
+plot
